b5527e3fd30c563aef8fb445b7e7da5493eba3e9
dhanraj-cntrl <dhanraj.rajmane11@gmail.com>
2026-09-08 11:16:18 +0530

    samp

--- test_Cases/test_login.py
+++ test_Cases/test_login.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from PageObject.Login import Login
-import requests
 class Test_001_Login:
 
     username= "Admin"
