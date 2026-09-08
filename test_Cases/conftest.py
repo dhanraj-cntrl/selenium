9c1c8c4ccb0e73b781f86df9e5c63c88de8b2455
dhanraj-cntrl <dhanraj.rajmane11@gmail.com>
2026-09-08 11:31:19 +0530

    test

--- test_Cases/conftest.py
+++ test_Cases/conftest.py
@@ -1,32 +1,32 @@
 import pytest
 from selenium import webdriver
 
-# @pytest.fixture(params=["Chrome","Edge"])
-# def setup(request):
-#     browser=request.param
-#
-#     if browser== "Chrome":
-#         options=webdriver.ChromeOptions()
-#         options.add_argument("--headless")
-#         driver=webdriver.Chrome(options=options)
-#     elif browser == "Edge":
-#         options=webdriver.EdgeOptions()
-#         options.add_argument("--headless")
-#         driver=webdriver.Edge(options=options)
-#     driver.maximize_window()
-#     yield driver
-#     driver.quit()
-
 @pytest.fixture(params=["Chrome","Edge"])
 def setup(request):
     browser=request.param
+
     if browser== "Chrome":
-        driver=webdriver.Chrome()
+        options=webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        driver=webdriver.Chrome(options=options)
     elif browser == "Edge":
-        driver=webdriver.Edge()
+        options=webdriver.EdgeOptions()
+        options.add_argument("--headless")
+        driver=webdriver.Edge(options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
+
+# @pytest.fixture(params=["Chrome","Edge"])
+# def setup(request):
+#     browser=request.param
+#     if browser== "Chrome":
+#         driver=webdriver.Chrome()
+#     elif browser == "Edge":
+#         driver=webdriver.Edge()
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
 
 
 
