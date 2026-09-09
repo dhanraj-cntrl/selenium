ede2ac56ed81705892fce40cde464b7a11e04012
dhanraj-cntrl <dhanraj.rajmane11@gmail.com>
2026-09-09 09:21:55 +0530

    test

--- PageObject/SauceDemoPageObject.py
+++ PageObject/SauceDemoPageObject.py
@@ -8,7 +8,9 @@ class SauceDemo:
     sauce_uname = "//*[@placeholder='Username']"
     sauce_pass="//*[@placeholder='Password']"
     sauce_click_button="//*[@class='submit-button btn_action']"
-
+    saurce_item = "//*[contains(text(),'Sauce Labs Backpack')]"
+    sauce_item_add_to_cart="(//button[contains(text(),'Add to cart')])[1]"
+    sauce_check_item="//*[@class='shopping_cart_badge']"
 
     def __init__(self,driver):
         self.driver=driver
@@ -21,6 +23,19 @@ class SauceDemo:
 
     def sauce_login(self):
         self.driver.find_element(By.XPATH,self.sauce_click_button).click()
+
+    def set_sauceitem(self):
+        self.driver.find_element(By.XPATH,self.saurce_item).click()
+
+    def set_saiceitemaddtokart(self):
+        self.driver.find_element(By.XPATH,self.sauce_item_add_to_cart).click()
+
+    def check_kart(self):
+        self.driver.find_element(By.XPATH,self.sauce_check_item).click()
+
+
+
+
 
 
 
--- test_Cases/test_SaurceDemo.py
+++ test_Cases/test_SaurceDemo.py
@@ -21,6 +21,32 @@ class Test_101_Saurcedemo:
         self.ssob1.sauce_login()
         assert self.driver.title == "Swag Labs"
 
+    def test_sauce_demo_addtokart(self,setup):
+        self.driver=setup
+        self.driver.get(self.ss_url)
+        self.ssob1=SauceDemo(self.driver)
+        self.wait = WebDriverWait(self.driver, 20)
+        self.wait.until(EC.presence_of_element_located((By.XPATH, self.ssob1.sauce_uname)))
+        self.ssob1.sauce_set_username(self.ss_username)
+        self.ssob1.sauce_set_password(self.ss_password)
+        self.ssob1.sauce_login()
+        wait=WebDriverWait(self.driver,20)
+        wait.until(EC.presence_of_element_located((By.XPATH,self.ssob1.saurce_item)))
+        self.ssob1.set_sauceitem()
+        self.ssob1.set_saiceitemaddtokart()
+        self.ssob1.check_kart()
+        self.driver.save_screenshot("AddtoKart.png")
+        
+
+
+
+
+
+
+
+
+
+
 
 
 
